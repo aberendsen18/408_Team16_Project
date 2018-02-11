fbe25c47bcd0de6dce4e0a2fd98cf0fa35fa422b
Unknown <emerickt@purdue.edu>
2018-02-11 14:47:55 -0500

    Added a shoppingList repo test class

---
+++ 14Cup/app/src/test/java/com/moufee/a14cup/repository/ShoppingListRepositoryTest.java
@@ -0,0 +1,15 @@
+package com.moufee.a14cup.repository;
+
+import org.junit.Test;
+
+/**
+ * Created by tyler on 2/11/18.
+ */
+
+public class ShoppingListRepositoryTest {
+
+    @Test
+    public void createNewShoppingList() throws Exception{
+        
+    }
+}

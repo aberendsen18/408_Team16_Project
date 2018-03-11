4eb518a7ab75ca60dc2f564c1eed92bbbdefce73
Tyler Emerick <emerickt@purdue.edu>
2018-03-11 16:03:36 -0400

    Added RecipesList class and created instance in RecipeViewModel

---
+++ 14Cup/app/src/main/java/com/moufee/a14cup/recipes/RecipesList.java
@@ -0,0 +1,20 @@
+package com.moufee.a14cup.recipes;
+
+import java.util.List;
+
+/**
+ * Created by tyler on 3/11/18.
+ */
+
+public class RecipesList {
+
+    public List<Recipe> recipeList;
+
+    public List<Recipe> getRecipeList() {
+        return recipeList;
+    }
+
+    public void setRecipeList(List<Recipe> recipeList) {
+        this.recipeList = recipeList;
+    }
+}
--- 14Cup/app/src/main/java/com/moufee/a14cup/ui/recipes/RecipeViewModel.java
+++ 14Cup/app/src/main/java/com/moufee/a14cup/ui/recipes/RecipeViewModel.java
@@ -9,6 +9,7 @@ import android.arch.lifecycle.ViewModel;
 import com.google.firebase.auth.FirebaseUser;
 import com.moufee.a14cup.lists.ShoppingList;
 import com.moufee.a14cup.recipes.Recipe;
+import com.moufee.a14cup.recipes.RecipesList;
 import com.moufee.a14cup.repository.RecipeRepository;
 import com.moufee.a14cup.repository.ShoppingListRepository;
 import com.moufee.a14cup.repository.UserRepository;
@@ -30,6 +31,7 @@ public class RecipeViewModel extends ViewModel {
     private LiveData<List<ShoppingList>> mListLiveData;
     //private Recipe mSelectedRecipe;
     private MutableLiveData<Recipe> mSelectedRecipe = new MutableLiveData<>();
+    private MutableLiveData<RecipesList> mRecipesList = new MutableLiveData<>();
     private LiveData<FirebaseUser> mCurrentUser;
 
     @Inject
@@ -57,6 +59,14 @@ public class RecipeViewModel extends ViewModel {
     // Store the state of the selected recipe from the RecipeFragment
     public void setSelectedRecipe(Recipe recipe){
         mSelectedRecipe.setValue(recipe);
+    }
+
+    public void setRecipesList(RecipesList recipeList){
+        mRecipesList.setValue(recipeList);
+    }
+
+    public LiveData<RecipesList> getRecipesList(){
+        return mRecipesList;
     }
 
     public Recipe getSelectedRecipe(){
